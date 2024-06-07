12799a31a3d9aef1e5b0ddca19d83abe46abaf14
Kostiantyn <k.varetsa@gmail.com>
2024-06-07 15:13:19 +0200

    Added project's applications

---
+++ spanish_app/admin_and_moderation/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AdminAndModerationConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'admin_and_moderation'
---
+++ spanish_app/search_and_recommendation/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SearchAndRecommendationConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'search_and_recommendation'
