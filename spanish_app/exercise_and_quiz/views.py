caf5088234c3114aa9a2f6ca29f36c14b0c1962b
Kostiantyn <k.varetsa@gmail.com>
2024-06-15 17:03:09 +0200

    Added basic templates for some apps in the project.

--- spanish_app/exercise_and_quiz/views.py
+++ spanish_app/exercise_and_quiz/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+from exercise_and_quiz.models import Exercises
 
 
 # Create your views here.
 def exercise_index(request):
-    return HttpResponse("Exercise test page.")
+    exercises = Exercises.objects.filter(lesson_id=1)
+    context = {'title': 'Exercises page', 'exercises': exercises}
+    return render(request, 'exercise_and_quiz/exercises.html', context)
