feb40e5e8c53d7394e3f7324b022332028e04dd9
Kostiantyn <k.varetsa@gmail.com>
2024-06-17 11:43:09 +0200

    Refactor templates folder into project root directory

--- spanish_app/spanish_app/settings.py
+++ spanish_app/spanish_app/settings.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'spanish_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
